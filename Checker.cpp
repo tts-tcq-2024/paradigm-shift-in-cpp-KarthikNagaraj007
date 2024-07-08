3ccf2ac10d3d048e99c27e3199b8e34800e45037
KarthikNagaraj007 <82349176+KarthikNagaraj007@users.noreply.github.com>
2024-07-08 12:33:25 +0530

    Update Checker.cpp

--- Checker.cpp
+++ Checker.cpp
@@ -33,8 +33,3 @@ bool isChargeRateOk(float chargeRate) {
 bool batteryIsOk(float temperature, float soc, float chargeRate) {
   return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
 }
-
-int main() {
-  assert(batteryIsOk(25, 70, 0.7) == true);
-  assert(batteryIsOk(50, 85, 0) == false);
-}
