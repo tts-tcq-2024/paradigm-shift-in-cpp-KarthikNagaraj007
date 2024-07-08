aaca82c202cefdb931bef7340c96f6b209dcddbe
KarthikNagaraj007 <82349176+KarthikNagaraj007@users.noreply.github.com>
2024-07-08 12:36:08 +0530

    Update unittest_checker.cpp

--- unittest_checker.cpp
+++ unittest_checker.cpp
@@ -19,35 +19,6 @@ void outputMessage(const std::string& message) {
     mockOutput->outputMessage(message);
 }
 
-// Function definitions
-bool isTemperatureOk(float temperature) {
-    if (temperature < 0 || temperature > 45) {
-        outputMessage("Temperature out of range!");
-        return false;
-    }
-    return true;
-}
-
-bool isSocOk(float soc) {
-    if (soc < 20 || soc > 80) {
-        outputMessage("State of Charge out of range!");
-        return false;
-    }
-    return true;
-}
-
-bool isChargeRateOk(float chargeRate) {
-    if (chargeRate > 0.8) {
-        outputMessage("Charge Rate out of range!");
-        return false;
-    }
-    return true;
-}
-
-bool batteryIsOk(float temperature, float soc, float chargeRate) {
-    return isTemperatureOk(temperature) && isSocOk(soc) && isChargeRateOk(chargeRate);
-}
-
 // Test fixture class
 class BatteryTest : public Test {
 protected:
